143e133092cdcb1c8e3614e8e4d778938942a970
purian <purian@gmail.com>
2014-10-13 16:51:48 +0300

    Model annotation

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: hotels
+#
+#  id         :integer          not null, primary key
+#  price      :integer
+#  city       :string(255)
+#  name       :string(255)
+#  image      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Hotel < ActiveRecord::Base
   attr_accessible :city, :image, :name, :price
 end
--- test/unit/hotel_test.rb
+++ test/unit/hotel_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: hotels
+#
+#  id         :integer          not null, primary key
+#  price      :integer
+#  city       :string(255)
+#  name       :string(255)
+#  image      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class HotelTest < ActiveSupport::TestCase
