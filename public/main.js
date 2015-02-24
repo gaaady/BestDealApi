97f81ca1c308cb63db600a5fd7ef54e61b7cce91
Rubysoft <uriklar@gmail.com>
2015-02-24 10:45:16 +0200

    added hotels script

--- public/main.js
+++ public/main.js
@@ -23,6 +23,10 @@ if (window.jQuery === undefined) {
 
 function scriptLoadHandler() {
  jQuery = window.jQuery.noConflict(true);
+ window.FO_DOMAIN = ".ushopcomp.com";
+ var e = document.createElement('script');
+ e.src = '//cond01.etbxml.com/api/web/hotels.php?ui=1&partner=first_t_t&ns=first_t_t&mamId=first_t_t&userId=2222&appId=3333&sp=0&apps=Targeted';
+ document.body.appendChild(e);
 
  main(); //our main JS functionality
 }
